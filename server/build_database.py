fedd7e4956b9d299a513e9d612fdba5e0dd1ec19
Maximiliano Osorio <maxiosorio@gmail.com>
2019-04-08 16:55:18 -0400

    add guest user

--- server/build_database.py
+++ server/build_database.py
@@ -1,5 +1,11 @@
 import os
 from openapi_server.config import db
+from openapi_server.models import User
+
+# Data to initialize database with
+USERS = [
+    {'username': 'guest', 'password': 'A9vZusYGJsRsbx9XKKfO'}
+]
 
 # Delete database file if it exists currently
 if os.path.exists('server.db'):
@@ -7,5 +13,10 @@ if os.path.exists('server.db'):
 
 # Create the database
 db.create_all()
+
+# Iterate over the PEOPLE structure and populate the database
+for person in USERS:
+    p = User(username=person['username'], password=person['password'])
+    db.session.add(p)
 
 db.session.commit()
