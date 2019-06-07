3fc01af91a74d3fff9c47c1204a6d9f3e14b7154
Maximiliano Osorio <maxiosorio@gmail.com>
2019-06-07 16:08:29 -0400

    Add: server support variablepresentation

--- server/openapi_server/models/__init__.py
+++ server/openapi_server/models/__init__.py
@@ -11,6 +11,7 @@ from openapi_server.models.model_configuration import ModelConfiguration
 from openapi_server.models.model_version import ModelVersion
 from openapi_server.models.parameter import Parameter
 from openapi_server.models.process import Process
+from openapi_server.models.standard_variable import StandardVariable
 from openapi_server.models.time_interval import TimeInterval
 from openapi_server.models.unit import Unit
 from openapi_server.models.user import User
