5f9f1fae40aed672207daf5ae39c6a18418e3670
Maximiliano Osorio <maxiosorio@gmail.com>
2019-06-16 18:50:47 -0400

    Fix: improve production server

--- server/init.sh
+++ server/init.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+sed -i "s/username_var/${MYSQL_USER}/" config.ini
+sed -i "s/password_var/${MYSQL_PASSWORD}/" config.ini
+sed -i "s/host_var/${MYSQL_HOST}/" config.ini
+sed -i "s/port_var/${MYSQL_PORT}/" config.ini
+sed -i "s/database_name_var/${MYSQL_DATABASE}/" config.ini
+
 python build_database.py
 python -m openapi_server
