f004397901f8fb4fd86510a3717c305be9bad57d
Maximiliano <maxiosorio@gmail.com>
2020-02-25 11:21:15 -0800

    add cache and region

--- server/openapi_server/__main__.py
+++ server/openapi_server/__main__.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
 import connexion
+from connexion.spec import Specification
 
 from openapi_server import encoder
+from openapi_server.cached import CachedSpecification
 
 
 def main():
     app = connexion.App(__name__, specification_dir='./openapi/')
     app.app.json_encoder = encoder.JSONEncoder
+    Specification.from_file = CachedSpecification.from_file
+
     app.add_api('openapi.yaml',
                 arguments={'title': 'Model Catalog'},
                 pythonic_params=False)
