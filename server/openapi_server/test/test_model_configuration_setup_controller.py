91353689adf118f8811454ac07c145cd7c6f6691
Maximiliano <maxiosorio@gmail.com>
2020-02-27 12:47:32 -0800

    add tests

---
+++ server/openapi_server/test/test_model_configuration_setup_controller.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+import unittest
+
+from flask import json
+from six import BytesIO
+
+from openapi_server.models.model_configuration_setup import ModelConfigurationSetup  # noqa: E501
+from openapi_server.test import BaseTestCase
+
+
+class TestModelConfigurationSetupController(BaseTestCase):
+    """ModelConfigurationSetupController integration test stubs"""
+
+
+
+if __name__ == '__main__':
+    unittest.main()
