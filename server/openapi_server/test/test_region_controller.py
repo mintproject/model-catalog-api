4f21dd0da3d2b1444a6a54540e126127d3644d8a
Maximiliano Osorio <maxiosorio@gmail.com>
2020-03-17 15:44:52 -0700

    add: test circular
    closes: #104

--- server/openapi_server/test/test_region_controller.py
+++ server/openapi_server/test/test_region_controller.py
@@ -32,6 +32,26 @@ class TestRegionController(BaseTestCase):
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
+    def test_id_get_circular(self):
+        """Test case for test_id_get_circular
+        Get a Region
+        """
+
+        resource_name = "Travis"
+        resource_uri = "https://w3id.org/okn/i/mint/Travis"
+        query_string = [('username', self.get_username)]
+        headers = {
+            'Accept': 'application/json',
+        }
+        response = self.client.open(
+            '/v1.4.0/regions/{id}'.format(id=resource_name),
+            method='GET',
+            headers=headers,
+            query_string=query_string)
+        self.logger.info("Response length {}".format(len(response.json)))
+        self.assertEqual(Region.from_dict(response.json).id, resource_uri)
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
 
 if __name__ == '__main__':
     unittest.main()
