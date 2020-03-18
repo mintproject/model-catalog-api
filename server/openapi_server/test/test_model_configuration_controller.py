6d25ef0d2a7b1b4a256d0cd755b9d7963ec8dff1
rohitmaurya <rsmart26.rm@gmail.com>
2020-03-17 18:26:56 -0700

    #103 Modified test case and now it is working

--- server/openapi_server/test/test_model_configuration_controller.py
+++ server/openapi_server/test/test_model_configuration_controller.py
@@ -100,35 +100,37 @@ class TestModelConfigurationController(BaseTestCase):
 
         self.assertEqual(dict(diff), {})
 
-    # def test_post_grid_no_equal(self):
-    #     input_file_path = self.input_test_directory / "model_configuration_without_id_grid_not_equal.json"
-    #     token_ = self.login()["access_token"]
-    #     data = read_json_file(input_file_path)
-    #     headers = {
-    #         'Content-Type': 'application/json',
-    #         'Authorization': "Bearer {}".format(token_),
-    #     }
-    #     response = self.client.open(
-    #         '/v1.4.0/modelconfigurations',
-    #         method='POST',
-    #         headers=headers,
-    #         data=json.dumps(data),
-    #         content_type='application/json')
-    #
-    #     self.assertEqual(response.status_code, 201)
-    #     model_request = ModelConfiguration.from_dict(data)
-    #     model_request.id = response.json['id']
-    #
-    #     ##### Verify if the request and response are equals
-    #     verification_query_string = [('username', "mosorio@isi.edu")]
-    #     verification_response = self.client.open(
-    #         '/v1.4.0/modelconfigurations/{id}'.format(id=model_request.id),
-    #         method='GET',
-    #         headers=headers,
-    #         query_string=verification_query_string)
-    #     self.logger.warning(model_request.id)
-    #     model_response = ModelConfiguration.from_dict(verification_response.json)
-    #     self.assertNotEqual(model_request.has_grid[0].id, model_response.has_grid[0].id)
+    def test_post_grid_no_equal(self):
+        input_file_path = self.input_test_directory / "model_configuration_without_id_causal_diagram_not_equal.json"
+        token_ = self.login()["access_token"]
+        data = read_json_file(input_file_path)
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': "Bearer {}".format(token_),
+        }
+        response = self.client.open(
+            '/v1.4.0/modelconfigurations',
+            method='POST',
+            headers=headers,
+            data=json.dumps(data),
+            content_type='application/json')
+
+        self.assertEqual(response.status_code, 201)
+        model_request = ModelConfiguration.from_dict(data)
+        model_request.id = response.json['id']
+
+        ##### Verify if the request and response are equals and id is generated for has_grid
+        verification_query_string = [('username', "mosorio@isi.edu")]
+        verification_response = self.client.open(
+            '/v1.4.0/modelconfigurations/{id}'.format(id=model_request.id),
+            method='GET',
+            headers=headers,
+            query_string=verification_query_string)
+        self.logger.warning(model_request.id)
+        model_response = ModelConfiguration.from_dict(verification_response.json)
+        self.assertNotEqual(model_request.has_causal_diagram[0].id, model_response.has_causal_diagram[0].id)
+        self.assertNotEqual(model_request.has_causal_diagram[1].id, model_response.has_causal_diagram[1].id)
+        self.logger.info("Id is generated for Causal Diagram {}".format(model_response.has_causal_diagram[0].id))
 
 if __name__ == '__main__':
     unittest.main()
