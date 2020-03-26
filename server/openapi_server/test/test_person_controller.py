ad6f22550000eb75b2acc14dc8f6442991ec6b89
Maximiliano Osorio <maxiosorio@gmail.com>
2020-03-25 18:49:40 -0700

    fix: add test not found

--- server/openapi_server/test/test_person_controller.py
+++ server/openapi_server/test/test_person_controller.py
@@ -73,5 +73,25 @@ class TestPersonController(BaseTestCase):
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
+
+    def test_persons_get_id_mint_not_found(self):
+        """Test case for persons_get
+
+        List all Person entities
+        """
+        query_string = [('username', 'mint@isi.edu')]
+
+        headers = {
+            'Accept': 'application/json',
+        }
+        response = self.client.open(
+            '/v1.4.0/persons/{}'.format("b0b454fc-d77d-4778-9d37-6e327c00b0c2"),
+            method='GET',
+            headers=headers,
+            query_string=query_string)
+        self.logger.info("Response length {}".format(len(response.json)))
+        self.assert404(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
 if __name__ == '__main__':
     unittest.main()
