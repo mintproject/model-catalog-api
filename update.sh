50719b03446db208d98a0fcd41aae808ef2a0af3
Maximiliano Osorio <maxiosorio@gmail.com>
2020-07-29 12:59:49 -0700

    fix: update

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 rsync --delete -av /Users/mosorio/repos/OBA/outputs/modelcatalog/servers/python/server/ server
-gco -- server/.env server/docker-compose.yml server/openapi_server/settings/config.ini server/openapi_server/test/__init__.py server/openapi_server/test/input_tests/model_configuration_without_id.json server/openapi_server/test/input_tests/model_configuration_without_id_causal_diagram_not_equal.json
+git checkout -- server/.env server/docker-compose.yml server/openapi_server/settings/config.ini server/openapi_server/test/ server/openapi_server/test/input_tests/model_configuration_without_id.json server/openapi_server/test/input_tests/model_configuration_without_id_causal_diagram_not_equal.json
 rm -rf server/openapi_server/controllers/default_controller.py server/.travis.yml server/.gitignore server/queries/queries/
 rsync --delete -av /Users/mosorio/repos/OBA/outputs/modelcatalog/servers/python/openapi.yaml openapi.yaml
