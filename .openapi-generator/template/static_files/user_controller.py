417b574a8f2ef880c962497a6b41dc737b9a1e0e
Maximiliano Osorio <maxiosorio@gmail.com>
2021-05-19 09:47:07 -0400

    fix: removing firebase links

--- .openapi-generator/template/static_files/user_controller.py
+++ .openapi-generator/template/static_files/user_controller.py
@@ -5,16 +5,11 @@ import json
 import requests
 from werkzeug.exceptions import Unauthorized
 
-from openapi_server.settings import FIREBASE_KEY
-JWT_ISSUER = 'com.zalando.connexion'
-JWT_SECRET = 'change_this'
-JWT_LIFETIME_SECONDS = 60000000
-JWT_ALGORITHM = 'HS256'
-
+from openapi_server.settings import AUTH_SERVER, AUTH_CLIENT_ID, AUTH_SECRET, JWT_ALGORITHM
 
 def decode_token(token):
     try:
-        return jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+        return jwt.decode(token, AUTH_SECRET, algorithms=[JWT_ALGORITHM])
     except JWTError as e:
         six.raise_from(Unauthorized, e)
 
@@ -22,22 +17,29 @@ def decode_token(token):
 def _current_timestamp() -> int:
     return int(time.time())
 
+
 def auth_with_password(email, password):
     headers = {
         'Sec-Fetch-Mode': 'cors',
         'Content-Type': 'application/json',
     }
 
-    params = (
-        ('key', FIREBASE_KEY),
+    data = json.dumps(
+        {
+            "username": email,
+            "password": password,
+            "grant_type": "password",
+            "client_id": AUTH_CLIENT_ID
+        }
     )
 
-    data = json.dumps({"email": email,"password": password,"returnSecureToken": True})
+    response = requests.post(
+        AUTH_SERVER,
+        headers=headers,
+        data=data)
 
-    response = requests.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword',
-                             headers=headers, params=params, data=data)
+    return response
 
-    return response.ok
 
 def user_login_get(username, password):  # noqa: E501
     """Logs user into the system
@@ -51,25 +53,7 @@ def user_login_get(username, password):  # noqa: E501
 
     :rtype: str
     """
-    if not auth_with_password(username, password):
+    response = auth_with_password(username, password)
+    if not response.ok:
         return "Invalid User or Password", 401, {}
-
-
-    timestamp = _current_timestamp()
-    payload = {
-        "iss": JWT_ISSUER,
-        "iat": int(timestamp),
-        "exp": int(timestamp + JWT_LIFETIME_SECONDS),
-        "sub": str(username),
-    }
-
-    access_token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
-    return {
-        "access_token": access_token,
-        "token_type": "bearer",
-        "expires_in": JWT_LIFETIME_SECONDS,
-        "refresh_token": "IwOGYzYTlmM2YxOTQ5MGE3YmNmMDFkNTVk",
-        "scope": "create"
-    }
-
-
+    return response.text
