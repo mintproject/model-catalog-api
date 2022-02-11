4e553bf6a291e1321eac1f520bf130d4d8ca27b0
Maximilinano Osorio <mosorio@isi.edu>
2022-02-11 12:03:36 -0300

    fix: use uswgi

--- server/openapi_server/__init__.py
+++ server/openapi_server/__init__.py
@@ -18,3 +18,20 @@ query_manager = QueryManager(
     context_dir=CONTEXT_DIRECTORY,
     named_graph_base=ENDPOINT_GRAPH_BASE,
     uri_prefix=ENDPOINT_RESOURCE_PREFIX)
+
+#!/usr/bin/env python3
+
+from openapi_server.cached import CachedSpecification
+from connexion.spec import Specification
+
+Specification.__init__ = CachedSpecification.__init__;
+Specification.from_file = CachedSpecification.from_file
+
+import connexion
+from openapi_server import encoder
+
+app = connexion.App(__name__, specification_dir='./openapi/')
+app.app.json_encoder = encoder.JSONEncoder
+app.add_api('openapi.yaml',
+            arguments={'title': 'model catalog'},
+            pythonic_params=False)
