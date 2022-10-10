3e5a849efd33a8bb401ba7a10bda704cdd6f4635
Maximilinano Osorio <mosorio@isi.edu>
2022-10-10 15:27:27 -0300

    update mustache

--- server/openapi_server/__init__.py
+++ server/openapi_server/__init__.py
@@ -1,12 +1,51 @@
-from obasparql import QueryManager
-from openapi_server.settings import *
 import logging
+import logging.config
+import os
+from obasparql import QueryManager
+from openapi_server.settings import ENDPOINT, ENDPOINT_GRAPH_BASE, ENDPOINT_PASSWORD, ENDPOINT_RESOURCE_PREFIX, ENDPOINT_USERNAME, QUERY_DIRECTORY, CONTEXT_DIRECTORY
 
-try:
-    logging.config.fileConfig(logging_file)
-except:
-    logging.error("Logging config file does not exist {}".format(logging_file))
-    exit(0)
+# Disable Django's logging setup
+LOGGING_CONFIG = None
+
+LOGLEVEL = os.environ.get('LOGLEVEL', 'info').upper()
+
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'default': {
+            # exact format is not important, this is the minimum information
+            'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+        },
+    },
+    'handlers': {
+        # console logs to stderr
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'default',
+        },
+    },
+    'loggers': {
+        # default for all undefined Python modules
+        '': {
+            'level': 'WARNING',
+            'handlers': ['console'],
+        },
+        # Our application code
+        'openapi_server': {
+            'level': LOGLEVEL,
+            'handlers': ['console'],
+            # Avoid double logging because of root logger
+            'propagate': False,
+        },
+        # Prevent noisy modules from logging to Sentry
+        'noisy_module': {
+            'level': 'ERROR',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+    },
+})
 
 logger = logging.getLogger(__name__)
 
